5e1ecd4c536dd4db9432b00d153fb619ea3cedfc
Bianca T <57296028+biancaatsh@users.noreply.github.com>
2025-04-29 17:03:41 +0000

    bianca

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 export PATH=$PATH:/usr/bin
 
 # Pull the Docker image
-docker pull abhishekf/simple-python-flask-app
+docker pull bianca22a/simple-python-flask-app
 
 # Run the container
-docker run -d -p 5000:5000 abhishekf/simple-python-flask-app
+docker run -d -p 5000:5000 bianca22a/simple-python-flask-app
