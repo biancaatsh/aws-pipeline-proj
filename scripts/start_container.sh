10168d5b951192f8b8465a0cdedba5aa8c4ae3d2
Bianca T <57296028+biancaatsh@users.noreply.github.com>
2025-04-29 19:53:52 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
+export PATH=$PATH:/usr/bin
 
-# Pull the Docker image from Docker Hub
-echo
-# Run the Docker image as a container
+# Pull the Docker image
+docker pull abhishekf/simple-python-flask-app
+
+# Run the container
+docker run -d -p 5000:5000 abhishekf/simple-python-flask-app
