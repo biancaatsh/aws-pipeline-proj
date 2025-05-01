d3475a1b07e140f0df747836ca37a3ff7e828954
Bianca T <57296028+biancaatsh@users.noreply.github.com>
2025-05-01 13:20:29 +0300

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # Stop the running container by image name
-CONTAINER_ID=$(docker ps -q --filter ancestor=bianca22a/simple-python-flask-app)
-
-if [ -n "$CONTAINER_ID" ]; then
-  docker stop "$CONTAINER_ID"
-else
-  echo "No container found to stop"
-fi
+echo "Bianca"
